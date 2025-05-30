0eefde1f9c47c21da91c4cbe260fae0c2887c911
geoson <balsohn@naver.com>
2025-05-30 11:36:04 +0900

    init: 프로젝트 디렉토리 êµ 구조

---
+++ src/main/java/com/example/picflow/PicflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.picflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/picflow/PicflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
